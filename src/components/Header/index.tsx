56891200d77b83e16cabec5248f0a8e52abf71f9
Welder n. Fernandes <welderx3@gmail.com>
2022-02-24 23:05:48 -0300

    feat: add dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
+import { Switch } from "@headlessui/react";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 import { CgMoon, CgSun } from "react-icons/cg";
 
 export default function Header() {
 	const { theme, setTheme } = useTheme();
-
+	const [enabled, setEnabled] = useState(false);
 	function handleDarkMode() {
 		setTheme(theme === "light" ? "dark" : "light");
 	}
@@ -11,17 +13,50 @@ export default function Header() {
 	return (
 		<div className="container flex items-center justify-between h-16 mb-2">
 			<h1 className="text-2xl font-bold">Welder</h1>
-			<ul className="flex ">
-				<li>
-					<button type="button" onClick={handleDarkMode}>
-						{theme === "light" ? (
-							<CgMoon className="text-2xl" />
-						) : (
-							<CgSun className="text-2xl" />
-						)}
-					</button>
-				</li>
-			</ul>
+
+			<Switch
+				checked={enabled}
+				onChange={handleDarkMode}
+				className={`${
+					theme === "light"
+						? "bg-gradient-to-l dark:bg-yellow-500 bg-black text-white"
+						: "bg-yellow-300 dark:text-gray-300 text-gray-800"
+				}
+          					relative 
+							items-center
+							px-1
+							inline-flex 
+							justify-between
+							flex-shrink-0 
+							h-[30px] 
+							w-[70px] 
+							border-2 
+							border-transparent 
+							rounded-full 
+							cursor-pointer 
+							transition-colors 
+							ease-in-out duration-200 
+							focus:outline-none 
+							focus-visible:ring-2  
+							focus-visible:ring-white
+							focus-visible:ring-opacity-75`}
+			>
+				<span className="sr-only">Use setting</span>
+				<CgSun className="text-xl " />
+				<CgMoon className="text-2xl " />
+
+				<span
+					aria-hidden="true"
+					className={`${theme === "light" ? "translate-x-9" : "translate-x-0 "}
+            				 pointer-events-none 
+							 absolute
+							 inline-block 
+							 h-[24px] 
+							 w-[24px] 
+							 rounded-full 
+							 bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+				/>
+			</Switch>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
 		"./src/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				shape: "#212121",
+				sun: "#f9d71c",
+				gray: {
+					light: "#535353",
+				},
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+				dm: ["DM Sans", "sans-serif"],
+			},
+			fontWeight: {
+				light: "300",
+				regular: "400",
+				medium: "500",
+				bold: "700",
+			},
+		},
 	},
 	plugins: [],
 };
